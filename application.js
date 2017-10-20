e534a79a848683f470b554a412360c69acfb8c6f
Brian Gallagher <brian.gallagher@feedhenry.com>
2017-10-20 15:38:51 +0100

    add auth

--- application.js
+++ application.js
@@ -1,18 +1,48 @@
 'use strict';
 
-let express = require('express');
-let cors = require('cors');
-let dbConnection = require('./lib/models/dbConnection');
-let app = express();
-let logger = require('./lib/logger');
-
+const express = require('express');
+const cors = require('cors');
+const dbConnection = require('./lib/models/dbConnection');
+const app = express();
+const logger = require('./lib/logger');
+const https = require('https');
+const fs = require('fs');
+const passport = require('passport');
+const authInit = require('./lib/services/authInit');
+var ejs = require('ejs');
 
 // Enable CORS for all requests
-app.use(cors());
+app.use(cors({credentials: true}));
 
 // allow serving of static files from the public directory
 app.use(express.static(__dirname + '/public'));
 
+app.all('*', function(req, res, next) {
+  console.log('setting Access-Control-Allow-Origin header: ' + req.headers.origin || req.headers.referer );
+
+  res.header('Access-Control-Allow-Origin', req.headers.origin || req.headers.referer);
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  next();
+});
+
+// TODO: not sure if this is needed
+app.use(require('method-override')());
+// TODO:
+// See https://github.com/expressjs/session
+// store
+// The session store instance, defaults to a new MemoryStore instance.
+app.use(require('express-session')({
+  secret: 'wYAEJhxEDTkDqmHFr7EDY2GLybLuCQ',
+  // cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+authInit.init();
+
+app.set('views', __dirname + '/lib/views');
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs-locals'));
 
 app.use('/auth', require('./lib/routes/auth.js')());
 app.use('/businessgoals', require('./lib/routes/businessGoals.js')());
@@ -33,19 +63,39 @@ app.use('/sys/info/ping', require('./lib/routes/ping.js')());
 // TODO:
 // Add error handling middleware
 
+var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-// TOOD: need to remove process.env.FH_PORT
-let port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
-let host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const options = {
+  key: fs.readFileSync('./privateKey.pem'),
+  cert: fs.readFileSync('./server.crt')
+};
 
-app.listen(port, host, function() {
-  dbConnection.connect(function (err) {
-    if (err) {
-      logger.error('Exiting app, failed to connect to Mongo DB');
-      process.exit();
-    }
-    else {
-      logger.info('App started at: ' + new Date() + ' on port: ' + port);
-    }
-  });
+https.createServer(options, app).listen(port);
+
+dbConnection.connect(function (err) {
+  if (err) {
+    logger.error('Exiting app, failed to connect to Mongo DB');
+    process.exit();
+  }
+  else {
+    logger.info('App started at: ' + new Date() + ' on port: ' + port);
+  }
 });
+
+
+// // TOOD: need to remove process.env.FH_PORT
+// let port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+// let host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+// app.listen(port, host, function() {
+//   dbConnection.connect(function (err) {
+//     if (err) {
+//       logger.error('Exiting app, failed to connect to Mongo DB');
+//       process.exit();
+//     }
+//     else {
+//       logger.info('App started at: ' + new Date() + ' on port: ' + port);
+//     }
+//   });
+// });
